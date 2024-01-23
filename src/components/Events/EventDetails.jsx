e080b0fd1f419951d66a09958c858711af86af9b
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-23 12:45:57 -0500

    399 mi solucion al challenge planteado en 398

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,77 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { deleteEvent, fetchEvent, queryClient } from "../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+
+// react router's useParams Hook para traer la data de edit
+// useMutation para borrar el evento
 
 export default function EventDetails() {
+  
+  const params = useParams();
+  const id = params.id;
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["evento", { eventId: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+    enabled: true
+  });
+  
+  const navigate = useNavigate();
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['events'] });
+      navigate('/events');
+    }
+  });
+
+  function handleDelete(){
+    console.log('EventDetails.jsx ... handleDelete id: ' + id);    
+    mutate({id});
+  }
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="Error al traer los datos del evento"
+        message={error.info?.message || "Error al traer evento"}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <article id="event-details">
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt="" />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} @ {data.time}</time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </article>
+    );
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +80,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
+      {content}
     </>
   );
 }
