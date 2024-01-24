0697117dc3216e639a24f47212f1485a2f109a56
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-24 09:39:01 -0500

    404 Optimistic Updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { useQuery, useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent, updateEvent } from "../util/http.js";
+import { fetchEvent, updateEvent, queryClient } from "../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -18,11 +18,28 @@ export default function EditEvent() {
 
   const { mutate } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+      const previousEvent = queryClient.getQueryData(["events", params.id]);
+
+      queryClient.setQueryData(["events", params.id], newEvent);
+
+      return { previousEvent }
+    },
+    onError: (error, data, context) => {
+      queryClient.setQueryData(['events', params.id], context.previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]);
+    }
   });
 
   function handleSubmit(formData) {
     mutate({ id: params.id, event: formData });
-    navigate('../');
+
+    navigate("../");
   }
 
   function handleClose() {
@@ -59,19 +76,17 @@ export default function EditEvent() {
   }
 
   if (data) {
-    content = <EventForm inputData={data} onSubmit={handleSubmit}>
-    <Link to="../" className="button-text">
-      Cancel
-    </Link>
-    <button type="submit" className="button">
-      Update
-    </button>
-  </EventForm>;
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
+        <Link to="../" className="button-text">
+          Cancel
+        </Link>
+        <button type="submit" className="button">
+          Update
+        </button>
+      </EventForm>
+    );
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      {content}
-    </Modal>
-  );
+  return <Modal onClose={handleClose}>{content}</Modal>;
 }
